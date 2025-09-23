99c67679000a92490e03e7355550cd91dafc2c55
shubh <shubhbajpai7.com>
2025-09-24 01:18:30 +0530

    Prepare for Render deployment

--- app.py
+++ app.py
@@ -1,46 +1,32 @@
-from flask import Flask,request,render_template
-import numpy as np
-import pandas as pd
+from flask import Flask, request, render_template
+from flask_cors import CORS
+from src.mlproject.pipeline.predict_pipeline import CustomData, PredictPipeline
 
-from sklearn.preprocessing import StandardScaler
-from src.mlproject.pipeline.predict_pipeline import CustomData,PredictPipeline
-
-application=Flask(__name__)
-
-app=application
-
-## Route for a home page
+app = Flask(__name__)
+CORS(app)  # For all origins during development
 
 @app.route('/')
 def index():
-    return render_template('index.html') 
+    return render_template('index.html')
 
-@app.route('/predictdata',methods=['GET','POST'])
+@app.route('/predictdata', methods=['GET', 'POST'])
 def predict_datapoint():
-    if request.method=='GET':
+    if request.method == 'GET':
         return render_template('home.html')
     else:
-        data=CustomData(
+        data = CustomData(
             gender=request.form.get('gender'),
             race_ethnicity=request.form.get('ethnicity'),
             parental_level_of_education=request.form.get('parental_level_of_education'),
             lunch=request.form.get('lunch'),
             test_preparation_course=request.form.get('test_preparation_course'),
-            reading_score=float(request.form.get('writing_score')),
-            writing_score=float(request.form.get('reading_score'))
-
+            reading_score=float(request.form.get('reading_score')),
+            writing_score=float(request.form.get('writing_score'))
         )
-        pred_df=data.get_data_as_data_frame()
-        print(pred_df)
-        print("Before Prediction")
+        pred_df = data.get_data_as_data_frame()
+        predict_pipeline = PredictPipeline()
+        results = predict_pipeline.predict(pred_df)
+        return render_template('home.html', results=results[0])
 
-        predict_pipeline=PredictPipeline()
-        print("Mid Prediction")
-        results=predict_pipeline.predict(pred_df)
-        print("after Prediction")
-        return render_template('home.html',results=results[0])
-    
-
-if __name__=="__main__":
-    app.run(host="0.0.0.0")        
-
+if __name__ == "__main__":
+    app.run(host="0.0.0.0")
